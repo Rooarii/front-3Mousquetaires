05656a0ab62a7d4e6c1c850793c3b9f73833360c
Alexandre-Dietsch <dietschalex@gmail.com>
2021-01-14 06:02:03 +0100

    header updated: logo artilect inserted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import userpicture from '../images/userpicture.jpg';
+import artilectlogo from '../images/artilectlogo.png'
 
 export default function Navbar() {
   return(
     <div className="header-container">
-      <div className="picture-profil">
+      <div className="profil">
         <img src={userpicture} alt='userpicture' />
       </div>
       <div className="artilect-name">
         <h1>Artilect</h1>
+        {/* <i class="fas fa-bars" /> */}
+        <img src={artilectlogo} alt='manu' />
       </div>
       <div className="burger-container">
       </div>
