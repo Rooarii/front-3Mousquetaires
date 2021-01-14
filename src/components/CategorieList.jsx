39bd3e497a7382269ef6b4ef1fe98d3db3fc8f33
Alexandre-Dietsch <dietschalex@gmail.com>
2021-01-14 10:32:43 +0100

    new disposition of home page

--- src/components/CategorieList.jsx
+++ src/components/CategorieList.jsx
@@ -7,18 +7,20 @@ import { Link} from 'react-router-dom';
 const CategorieList = () => {
   return(
     <div className="categorie-list-container">
-    <h1>Let's Play</h1>
+    <h1>Let's <span className='play'>Play</span></h1>
     <div className="main-container">
         {data.categories.map((item, index) => (
           
         <div className="container" key={index}>
-        <div className="parent" >
-        <Link to="/category"><h3 style={{color : `${item.color}` }}>{item.name.slice(0, 2).toUpperCase()}</h3></Link>
+          <div className="parent" >
+            <Link to="/category"><h3 style={{color : `${item.color}` }}>{item.name.slice(0, 2).toUpperCase()}</h3></Link>
+          </div>
+          <h2>{item.name.toUpperCase()}</h2>
         </div>
-        <h2>{item.name.toUpperCase()}</h2>
-            </div>
-           
       ))}
+      <div className="scroll-indication">
+        <i class="fas fa-angle-double-down" />
+      </div>
     </div>
     </div>
   )
